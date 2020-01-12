f152bb945c77730636a0e19792e83bf31a8b3acb
ennguyennguyen <ennguyennguyen@gmail.com>
2020-01-12 00:38:38 -0600

    update name URL and template extend

--- djangoblog/articles/urls.py
+++ djangoblog/articles/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    url(r'^$', views.article_list)
+    url(r'^$', views.article_list, name = "list"),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_details, name = "details"), # [NGUYEN] ^ means from the start, \w- means any length of number/letter
 ]
--- djangoblog/articles/views.py
+++ djangoblog/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 # [NGUYEN] this is the file to control what the viewer will see when accessing the Article page
 
@@ -11,3 +12,6 @@ def article_list(request):
 
     # update to display articles objects using a dictionary {'articles': articles}
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_details(request, slug):
+    return HttpResponse(slug)
